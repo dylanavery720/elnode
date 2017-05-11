3e71fc9c667b38dcfcbd0807319beb86c9fd5940
Dylan Avery <fossilfuelmusic@gmail.com>
2017-05-11 10:58:25 -0600

    One left, seve a public HTML

--- server.js
+++ server.js
@@ -57,6 +57,21 @@ methods.DELETE = function(path, respond) {
   });
 };
 
+methods.MKCOL = function(path, respond) {
+  fs.stat(path, function(error, stats) {
+    if (error && error.code == "ENOENT")
+      fs.mkdir(path, respondErrorOrNothing(respond))
+    else if (error)
+      respond(500, error.toString());
+    else if (stats.isDirectory())
+      respond(204);
+    else
+      respond(400, "File exists");
+  });
+};
+
+
+
 function respondErrorOrNothing(respond) {
   return function(error) {
     if (error)
